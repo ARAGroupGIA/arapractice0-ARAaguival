295fbacc6f4a224a1c7f13c991d44ab8735b15a8
ARAaguival <55189085+ARAaguival@users.noreply.github.com>
2019-09-11 14:15:16 +0200

    Exercise2.1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,13 @@
 /* Here, you should entry the required libraries */
-
+#include<stdio.h>
 void main(){
 	/* Here, you should write the source code of your program */
+	int number1;
+	printf("Give me a number: ");
+	scanf("%d", &number1);
+	int number2;
+	printf("Give another number: ");
+	scanf("%d", &number2);
+	int sum=number1+number2;
+	printf("The sum is %d", sum);
 }
