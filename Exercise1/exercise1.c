76b0116539007fe19e09ba73036eeb61fc6c3fa7
ARAaguival <55189085+ARAaguival@users.noreply.github.com>
2019-09-11 14:03:25 +0200

    Exercise1.1

--- Exercise1/exercise1.c
+++ Exercise1/exercise1.c
@@ -3,7 +3,8 @@
 /*************************************************************************/
 
 /* Here, you must include the required libraries */
-
-void main(){
+#include <stdio.h>
+void main() {
 	/* Here, you must write the source code to show the missatge "Hello world" on screen*/
+	printf("Hello word");
 }
