3a918f4d1182a001173fc732d0c6d7ff835a94eb
ARAaguival <55189085+ARAaguival@users.noreply.github.com>
2019-09-11 14:16:59 +0200

    Exercise2.2

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -8,6 +8,9 @@ void main(){
 	int number2;
 	printf("Give another number: ");
 	scanf("%d", &number2);
-	int sum=number1+number2;
+	int number3;
+	printf("Give me another number: ");
+	scanf("%d", &number3);
+	int sum=number1+number2+number3;
 	printf("The sum is %d", sum);
 }
